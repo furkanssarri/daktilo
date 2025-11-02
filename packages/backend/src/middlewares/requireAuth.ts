4a4ee07239906780c3006416b1b880ad9dc4b5f9
Furkan Sarı <furkansa@hotmail.com.tr>
2025-11-02 23:01:54 +0300

    refactor: type middlewares strongly

--- packages/backend/src/middlewares/requireAuth.ts
+++ packages/backend/src/middlewares/requireAuth.ts
@@ -1,25 +1,30 @@
 import passport from "passport";
 import type { Request, Response, NextFunction } from "express";
+import type { VerifiedCallback } from "passport-jwt";
 
 export const requireAuth = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  passport.authenticate("jwt", { session: false }, (err, user, info) => {
-    if (err) {
-      return res.status(500).json({ message: "Internal server error" });
-    }
+  passport.authenticate(
+    "jwt",
+    { session: false },
+    (err: unknown, user: Express.User | false, info: unknown) => {
+      if (err)
+        return res.status(500).json({ message: "Internal server error" });
 
-    if (!user) {
-      return res.status(401).json({
-        message: "Unauthorized. Invalid or missing token.",
-        details: info,
-      });
-    }
+      if (!user)
+        return res
+          .status(401)
+          .json({
+            message: "Unauthorized. Invalid or missing token.",
+            details: info,
+          });
 
-    // ✅ Attach authenticated user to the request
-    req.user = user;
-    next();
-  })(req, res, next);
+      // Attach authenticated user to the request
+      req.user = user;
+      next();
+    },
+  )(req, res, next);
 };
--- packages/backend/src/middlewares/requireRole.ts
+++ packages/backend/src/middlewares/requireRole.ts
@@ -3,7 +3,7 @@ import type { Request, Response, NextFunction } from "express";
 export const requireRole = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     // req.user is set by requireAuth
-    const userRole = req.user?.role; //TODO: .role
+    const userRole = req.user?.role;
 
     if (!userRole || !roles.includes(userRole)) {
       return res.status(403).json({
