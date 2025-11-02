00bd99f49eddbf7289d6fe3ffd1b3ed20415455a
Furkan Sarı <furkansa@hotmail.com.tr>
2025-11-02 14:58:41 +0300

    fix: ts compiler config

--- packages/backend/src/db/prismaClient.ts
+++ packages/backend/src/db/prismaClient.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in the .env");
+}
+
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
@@ -8,7 +12,7 @@ export const prisma =
     log: ["query", "info", "warn", "error"],
   });
 
-// prevent dupes
+// only attach once during HMR
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
